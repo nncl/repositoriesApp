c0706528e2626bc4a1d50c5536a33c0ccd155184
= <nncl@live.com>
2019-09-03 17:05:07 -0300

    Creating styled components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text } from 'react-native';
+import { Container } from './styles';
 
 export default function Main() {
-  return <View />;
+  return (
+    <Container>
+      <Text>Hello Users</Text>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
