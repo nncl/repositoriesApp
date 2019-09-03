d5945b1157f2c69f17f2ca01bd312a039fd2985d
= <nncl@live.com>
2019-09-03 18:08:16 -0300

    Designing form

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
   return (
     <Container>
-      <Text>Hello Users</Text>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Add user"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
     </Container>
   );
 }
