fcd9dfec5197cc3399901316c88f5c0bffe26765
= <nncl@live.com>
2019-09-04 01:10:04 -0300

    Listing starred repo's

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  Avatar,
+  Bio,
+  Name,
+  Stars,
+  Starred,
+  OnwerAvatar,
+  Info,
+  Title,
+  Author,
+  Spacer,
+} from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -16,17 +31,48 @@ export default class User extends Component {
 
   state = {
     stars: [],
+    loading: true,
   };
 
   async componentDidMount(): void {
     const { navigation } = this.props;
     const user = navigation.getParam('user');
     const response = await api.get(`/users/${user.login}/starred`);
-    this.setState({ stars: response.data });
+    this.setState({ stars: response.data, loading: false });
   }
 
   render() {
-    const { stars } = this.state;
-    return <View />;
+    const { navigation } = this.props;
+    const { stars, loading } = this.state;
+    const user = navigation.getParam('user');
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        {loading ? (
+          <Spacer>
+            <ActivityIndicator color="#915c91" />
+          </Spacer>
+        ) : (
+          <Stars
+            data={stars}
+            keyExtractor={star => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred>
+                <OnwerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
+      </Container>
+    );
   }
 }
